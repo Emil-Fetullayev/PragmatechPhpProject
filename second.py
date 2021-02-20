aced0f332754f8e80a997980074de62d3ee29953
Emil Fetullayev <69794670+Emil-Fetullayev@users.noreply.github.com>
2021-02-20 21:04:52 +0400

    Delete second.py

--- second.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self,_name,_surname):
-        self.Name=_name
-        self.Surname=_surname
-    def printFullName(self):
-        return f'Adiniz :{self.Name}, Soyadiniz :{self.Surname}'
